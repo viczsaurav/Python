6c4ed8a86586b741f6171f903a0be128549cf4f5
Saurav Verma <viczsaurav@gmail.com>
2015-04-13 21:12:28 +0800

    Palindrome Code

--- SkyScanner/CheckAndMakePalindrome.py
+++ SkyScanner/CheckAndMakePalindrome.py
@@ -5,11 +5,8 @@ def checkPalindrome(line):
     while(j>i):
         if line[i]!= line[j]:
             if line[i+1]!= line[j]:
-            	print "here"
                 return j
             if line[i]!= line[j-1]:
-                print "i = ", line[i-2],line[i-1], line[i], line[i+1],line[i+2]
-                print "j= ", line[j-2],line[j-1],line[j],line[j+1], line[j+2]
                 return i
 
         i+=1;j-=1
